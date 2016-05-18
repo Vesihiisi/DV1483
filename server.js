cbac455cec94ab9b6f14f3388f6f6dfefd5a8eed
Vesihiisi <alicia@fagerving.se>
2016-05-18 10:11:35 +0200

    some comments

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 // Require the modules we need
-var http =                require('http');
-var WebSocketServer =     require('websocket').server;
-var week =                require('week');
-var includes =            require('array-includes'); // check if array contains element
-var config =              require('./config'); // configuration settings
-var port =                config.port;
-var filename =            config.logFile; // log saved here
-var serverName =          config.serverName; // 'username' to sign server announcements
-var allowedOrins =        config.allowedOrigins; // only accept connections from these
-var Message =             require("./Message");
-var ConnectionGroup =     require("./ConnectionGroup");
+var http = require('http');
+var WebSocketServer = require('websocket').server;
+var week = require('week');
+var includes = require('array-includes'); // check if array contains element
+var config = require('./config'); // configuration settings
+var port = config.port;
+var filename = config.logFile; // log saved here
+var serverName = config.serverName; // 'username' to sign server announcements
+var allowedOrins = config.allowedOrigins; // only accept connections from these
+var Message = require("./Message"); // class for single message
+var ConnectionGroup = require("./ConnectionGroup"); // class for abstraction around all connected clients
 var WebSocketConnection = require("./WebSocketConnection");
 var i;
-
-
-
 
 
 // Create a http server with a callback handling all requests
@@ -23,10 +20,6 @@ var httpServer = http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-type': 'text/plain' });
     response.end('This is a chat server!\n');
 });
-
-
-
-
 
 
 // Always check and explicitly allow the origin
@@ -40,14 +33,11 @@ function originIsAllowed(origin) {
     return false;
 }
 
+
 // Setup the http-server to listen to a port
 httpServer.listen(port, function() {
     console.log((new Date()) + ' HTTP server is listening on port ' + port);
 });
-
-
-
-
 
 
 // Create an object for the websocket
@@ -58,12 +48,7 @@ wsServer = new WebSocketServer({
 });
 
 
-
-
-
-
-
-
+// Create array with all connected clients
 var allConnections = new ConnectionGroup([]);
 
 
@@ -78,13 +63,11 @@ wsServer.on('request', function(request) {
 
     var connection = request.accept('broadcast-protocol', request.origin);
 
-
     connection.sendOwnName();
     allConnections.add(connection);
-    allConnections.sendUsernames();
+    allConnections.sendUsernames(); // now the client can update list of participants
 
     console.log((new Date()) + ' Connection accepted from ' + request.origin);
-
 
 
     // Callback to handle each message from the client
@@ -100,17 +83,17 @@ wsServer.on('request', function(request) {
                 parts.shift();
                 var argument = parts.join(" "); // to be able to have whitespace inside argument, e.g. multiword user name
                 switch (command) {
-                    case "me":
+                    case "me": // user-initiated name setting request
                         connection.setUsername(argument, allConnections);
                         connection.sendOwnName();
                         allConnections.sendUsernames();
                         break;
-                    case "me_first":
+                    case "me_first": // the initial name setting request when connecting to server
                         connection.setUsername(argument, allConnections, "initial");
                         connection.sendOwnName();
                         allConnections.sendUsernames();
                         break;
-                    case "get_img":
+                    case "get_img": // automatic request for gravatar url
                         connection.setImg(argument);
                         connection.sendOwnName();
                         allConnections.sendUsernames();
@@ -126,10 +109,9 @@ wsServer.on('request', function(request) {
 
     // Callback when client closes the connection
     connection.on('close', function(reasonCode, description) {
-        var username = connection.username;
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-        allConnections.remove(connection);
-        allConnections.sendToAll(new Message(serverName, username + " has left.", "notification"));
+        allConnections.remove(connection); // make sure connection is no longer in the array
+        allConnections.sendToAll(new Message(serverName, connection.username + " has left.", "notification"));
         console.log("Active connections: " + allConnections.getNumber());
     });
 });
