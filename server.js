872f01565e54eff683d8495db04d593234e56c4c
Vesihiisi <alicia@fagerving.se>
2016-05-18 10:07:04 +0200

    rm doubled code

--- server.js
+++ server.js
@@ -13,17 +13,6 @@ var ConnectionGroup =     require("./ConnectionGroup");
 var WebSocketConnection = require("./WebSocketConnection");
 var i;
 
-// Convert Date object to unix timestamp
-// https://coderwall.com/p/rbfl6g/how-to-get-the-correct-unix-timestamp-from-any-date-in-javascript
-Date.prototype.getUnixTime = function() {
-    return this.getTime() / 1000 | 0
-};
-if (!Date.now) Date.now = function() {
-    return new Date();
-}
-Date.time = function() {
-    return Date.now().getUnixTime();
-}
 
 
 
