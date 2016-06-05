fcea20368188d0d396e0a344a00294a8d666fb48
Vesihiisi <alicia@fagerving.se>
2016-06-05 08:48:25 +0200

    add option to disallow certain names

--- WebSocketConnection.js
+++ WebSocketConnection.js
@@ -4,12 +4,23 @@
 var WebSocketConnection = require('websocket').connection;
 var Message = require("./Message");
 var gravatar = require('gravatar'); // get gravatar from email address
+var includes = require('array-includes'); // check if array contains element
 var config = require('./config'); // configuration settings
+var unavailableNames = config.unavailableNames; // you can't change to this name
 var serverName = config.serverName; // 'username' to sign server announcements
 var sanitize = require('sanitize-html'); // clean up user input to prevent injections
 
 WebSocketConnection.prototype.username = null;
 WebSocketConnection.prototype.img = null;
+
+WebSocketConnection.prototype.usernameIsOnTheProhibitedList = function(name, array) {
+    if (includes(array, name)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 WebSocketConnection.prototype.setUsername = function(name, group, mode) {
     mode = mode || null;
     name = name.trim() // whitespace is messy and causes confusion
@@ -21,14 +32,14 @@ WebSocketConnection.prototype.setUsername = function(name, group, mode) {
         allowedAttributes: []
     });
     if (mode === "initial") { // duplicate check
-        if (group.userExists(name)) { // if before chat starts, prevent duplicates by adding a number
+        if (group.userExists(name) || this.usernameIsOnTheProhibitedList(name, unavailableNames)) { // if before chat starts, prevent duplicates by adding a number
             name = name + "_2";
         }
         this.username = name;
         group.sendToAll(new Message(serverName, this.username + " has joined.", "notification"))
     } else { // but if a user attempts during chat, don't do anything, simply prevent
-        if (group.userExists(name)) {
-            this.sendToOne(new Message(serverName, "Can't change name", "warning"))
+        if (group.userExists(name) || this.usernameIsOnTheProhibitedList(name, unavailableNames)) {
+            this.sendToOne(new Message(serverName, "Name change failed.", "warning"))
         } else {
             var oldName = this.username;
             this.username = name;
